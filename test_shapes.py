bc613849bc1cd5b5739eb269677582163b16f64a
peterwi <peter@DIRAC.local>
2021-02-26 14:53:54 +0100

    Implemented Chan-Vese in Jupyter notebook.

--- test_shapes.py
+++ test_shapes.py
@@ -2,9 +2,11 @@
 
 from ClassFiles.dataGenerator import ShapeGenerator
 
-shapes = ShapeGenerator(512, 512)
+shapes = ShapeGenerator(128, 128)
 shapes.add_polygon(times=4)
 shapes.add_ellipse(times=5)
 
 shapes.add_noise()
-shapes.image.show()
+shapes.image.save("images/dirty.BMP")
+
+# shapes.image.show()
