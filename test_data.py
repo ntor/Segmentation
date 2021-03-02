46ee948f857d0000486ede6e86c14bafd1237245
Peter Wildemann <pw477@cam.ac.uk>
2021-03-02 10:49:24 +0100

    Modified 'test_data.py'

--- test_data.py
+++ test_data.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
 
-import torch
-import torchvision
+# import torch
+# import torchvision
 import matplotlib.pyplot as plt
+import ClassFiles.DataLoader as load
 # import numpy as np
 
 # This file is for browsing random samples of the artificially generated data.
 # We assume that the images lie in "./data/generated/[clean,dirty,chan-vese]".
 
-# Below we initialise a dataloader for the "clean data", loading the images into
-# tensors in minibatches.
 
-image_transform = torchvision.transforms.ToTensor()
-
-good_dataset = torchvision.datasets.ImageFolder("./data/generated/clean/", transform=image_transform)
-good_dataloader = torch.utils.data.DataLoader(good_dataset, batch_size=4, shuffle=True)
-
-good_dataiter = iter(good_dataloader)
+dataloader = load.get_generated_dataloader('clean')
+dataiter = iter(dataloader)
 
 for i in range(10):
-    sample_image = good_dataiter.next()
+    sample_image = dataiter.next()
     plt.imshow(sample_image.numpy())
     plt.show()
