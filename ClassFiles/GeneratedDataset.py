e02bc2bf3a5fe76db3d962da4aa3308f25b77574
Peter Wildemann <pw477@cam.ac.uk>
2021-03-10 17:04:06 +0100

    Fixed GenerateDataset and changed to new code from James.

--- ClassFiles/GeneratedDataset.py
+++ ClassFiles/GeneratedDataset.py
@@ -8,6 +8,30 @@ from ShapeGenerator import ShapeGenerator
 from ChanVese import ChanVese
 from tqdm import tqdm
 
+# This file implements two torch Dataset classes, 'ImageDataset' and
+# 'SegmentationDataset', and a function 'generate_data'. The datasets are
+# initialised with a path to a directory in which we assume a folder structure
+# as follows:
+#     + image_0
+#      - clean.png
+#      - dirty.png
+#      - clean_seg.npy
+#      - dirty_cv_seg.npy
+#    + image_1
+#      - clean.png
+#      - ...
+#
+# The dataloaders then provide an interface for loading, for example the clean
+# dirty images, or their segmentations.
+#
+# The actual names are not hardcoded but can be adjusted in the constants below.
+# The initialised Dataset instances are not to be used directly but rather in
+# order to initialise a torch DataLoader object.
+#
+# The 'generate_data' function can be used in order to populate a folder with
+# artifical data as described above.
+
+
 SAMPLE_FOLDER_PREFIX = "image_"
 
 IMAGE_TYPE_NAMES = {"dirty": "dirty.png", "clean": "clean.png"}
@@ -46,7 +70,7 @@ class SegmentationDataset(Dataset):
     def __init__(self, data_root, seg_type="dirty"):
         self.data_root = data_root
         self.seg_type = seg_type
-        if not os.path.isdir("data_root"):
+        if not os.path.isdir(data_root):
             print("ERROR: data_root is not a valid directory")
 
     def __len__(self):
@@ -65,13 +89,28 @@ class SegmentationDataset(Dataset):
         return seg
 
 
-def generate_data(times, root_dir, size=(128, 128)):
+def generate_data(times, root_dir, size=(128, 128), append=True):
+    if append:
+        start_index = sum(
+            1 for s in os.listdir(root_dir) if s.startswith(SAMPLE_FOLDER_PREFIX)
+        )
+    else:
+        start_index = 0
+
     for i in tqdm(range(times)):
-        sample_folder = os.path.join(root_dir, SAMPLE_FOLDER_PREFIX + "{}".format(i))
-        os.mkdir(sample_folder)
+        sample_folder = os.path.join(
+            root_dir, SAMPLE_FOLDER_PREFIX + "{}".format(i + start_index)
+        )
+        try:
+            os.mkdir(sample_folder)
+        except FileExistsError:
+            pass
+
         shapes = ShapeGenerator(128, 128)
-        shapes.add_polygon(times=np.random.randint(2, 6))
-        shapes.add_ellipse(times=np.random.ranint(2, 6))
+        shapes.add_polygon(times=np.random.randint(10, 40))
+        shapes.add_ellipse(times=np.random.randint(10, 40))
+        shapes.add_holes(numholes=np.random.randint(10, 40))
+
         shapes.image.save(
             fp=os.path.join(sample_folder, IMAGE_TYPE_NAMES["clean"]), format="PNG"
         )
@@ -79,7 +118,6 @@ def generate_data(times, root_dir, size=(128, 128)):
             file=os.path.join(sample_folder, SEGMENTATION_TYPE_NAMES["clean"]),
             arr=np.array(shapes.image) / 255,
         )
-        shapes.add_holes(np.random.randint(15, 60))
         shapes.add_noise()
         shapes.image.save(
             fp=os.path.join(sample_folder, IMAGE_TYPE_NAMES["dirty"]), format="PNG"
