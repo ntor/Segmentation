94b669ce29ec9d44b597ae144b1dbb2d18123127
jimmmmason <68656014+jimmmmason@users.noreply.github.com>
2021-03-14 20:36:05 +0000

    edit to array handling

--- EvaluationMetrics.py
+++ EvaluationMetrics.py
@@ -4,13 +4,15 @@ def Jaccard(u1,u2,threshold=0.8):
     intersection = 0
     union        = 0
     
-    u1.flatten
-    u2.flatten
+    u1 = np.reshape(u1,np.size(u1))
+    u2 = np.reshape(u2,np.size(u2))
     
     for i in range(len(u1)) :
-        if (u1[i] >threshold) and (u2[i]>threshold) :
+        a = u1[i]
+        b = u2[i]
+        if (a >threshold) and (b>threshold) :
             intersection += 1 
-        if (u1[i] >threshold) or  (u2[i]>threshold) :
+        if (a >threshold) or  (b>threshold) :
             union        += 1
     return intersection/union
 
@@ -20,15 +22,17 @@ def Sorensen(u1,u2,threshold=0.8):
     U1           = 0
     U2           = 0
     
-    u1.flatten
-    u2.flatten
+    u1 = np.reshape(u1,np.size(u1))
+    u2 = np.reshape(u2,np.size(u2))
     
     for i in range(len(u1)): 
-        if (u1[i] >threshold) and (u2[i]>threshold) :
+        a = u1[i]
+        b = u2[i]
+        if (a >threshold) and (b>threshold) :
             intersection += 1
-        if (u1[i] >threshold) :
+        if (a >threshold) :
             U1           += 1
-        if (u2[i] >threshold) :
+        if (b >threshold) :
             U2           += 1
         
     return 2*intersection/(U1+U2)
