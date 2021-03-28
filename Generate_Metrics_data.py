0dc8f006c18f03ced04ac59f40f6cbe38c4b48c5
jimmmmason <68656014+jimmmmason@users.noreply.github.com>
2021-03-28 21:04:46 +0100

    Add files via upload

--- Generate_Metrics_data.py
+++ Generate_Metrics_data.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
-import Neural_Networks_lunglike.ClassFiles.networks as net
+import ClassFiles.Networks as net
 import torch
 
 FILEPATH = "./data"
 
 
-NN = net.ConvNet1(1, 128, 128)
+NN = net.ConvNet8(1, 128, 128)
 NN.load_state_dict(
-    torch.load("./Neural_Networks_lunglike/ConvNet1_trained", map_location=torch.device("cpu") ))
-NetName = "ConnNet1"
+    torch.load("./Neural_Networks_lunglike/ConvNet8_trained", map_location=torch.device("cpu") ))
+NetName = "ConvNet8"
 
 
 from ClassFiles.GeneratedDatasetNN import generate_data_NN as gen
 
-gen(1000,FILEPATH,NN,NetName)
+gen(1,FILEPATH,NN,NetName)
